6ed478a984d7f68764dcf355c668de8420bafe55
Matt <cook6348@gmail.com>
2018-12-26 10:55:20 -0600

    Homework assignment 1

---
+++ HW1/songs.js
@@ -0,0 +1,75 @@
+// Homework Assignment #1
+
+// This homework assignment is about listing my favorite sessionStorage, and as many different attributes I can think of about the song
+
+//Fav Song name
+//String
+const favSong = "End of the Line";
+//Band name
+//String
+const bandName =  "Traveling Wilburys";
+//Year Released
+//Number
+const released = 1988;
+//Album Title
+//String
+const album = "Traveling Wilburys Vo1";
+//Genre of Music
+//Boolean
+const isRock = true;
+//Band Member names
+//An array was just the data structure needed to list out the names
+const bandMembers = ["Tom Petty", "George Harrison", "Bob Dylan", "Roy Orbison", "Jeff Lynne", "Jim Keltner"];
+//Number of songs on Album
+//Number
+const numberOfSongsOnAlbum = 12;
+//Album length
+//Number
+const albumLength = 36.22;
+//Length of favorite song
+//Number
+const songLength = 3.30;
+//Name and length of each song on the album
+//I chose an array of objects to display this information
+const lengthOfEachSong = [
+    {
+        name: "Handle with Care",
+        length: 3.19,
+    },
+    {
+        name: "Dirty World",
+        length: 3.30,
+    },{
+        name: "Rattled",
+        length: 3.00,
+    },{
+        name: "Last Night",
+        length: 3.48,
+    },{
+        name: "Not Alone Any More",
+        length: 3.24,
+    },{
+        name: "Congratulations",
+        length: 3.30,
+    },{
+        name: "Heading for the Light",
+        length: 3.37,
+    },{
+        name: "Margarita",
+        length: 3.15,
+    },{
+        name: "Tweeter and the Monkey Man",
+        length: 5.30,
+    },{
+        name: "End of the Line",
+        length: 3.30,
+    },{
+        name: "Maxine",
+        length: 2.49,
+    },{
+        name: "Like a Ship",
+        length: 3.31,
+    },
+]
+
+
